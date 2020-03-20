8a64c669385c74999cdd5af3b0b96b1e573235f5
frankenfurth <60515397+frankenfurth@users.noreply.github.com>
2020-03-20 18:37:46 -0500

    Add files via upload

--- DogeBot/DogeInfoCollect.bat
+++ DogeBot/DogeInfoCollect.bat
@@ -1,12 +1,33 @@
 @ECHO off
-setlocal EnableDelayedExpansion
+setlocal enabledelayedexpansion
 Title ArbitrageBot
 color 0A
+del closer.txt
 :CurlPage
 curl https://coinarbitragebot.com/price/dogecoin.html -o plinko.txt
 findstr /C:"arbitrage :" plinko.txt >>closer.txt
-type "closer.txt"|repl " " ";" >"closertwo.txt"
-del plinko.txt
+
+type closer.txt | repl " " "">>closer2.txt
 del closer.txt
+type closer2.txt | repl ">" "">>closer.txt
+del closer2.txt
+type closer.txt | repl "<" "">>closer2.txt
+del closer.txt
+type closer2.txt | repl "'" "">>closer.txt
+del closer2.txt
+type closer.txt | repl "_" "">>closer2.txt
+del closer.txt
+type closer2.txt | repl "|" "">>closer.txt
+del closer2.txt
+
+
+for /F "delims=" %%a in (closer.txt) do (
+    set /A count+=1
+    set "array[!count!]=%%a"
+)
+for /L %%i in (1,1,%count%) do echo !array[%%i]!
+
+
+
+del plinko.txt
 pause
-del closertwo.txt
